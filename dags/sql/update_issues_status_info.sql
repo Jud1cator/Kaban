605ed0d743b1fc3bedca4e38b1abf16b4f7df61a
Jud1cator <yarik200027@gmail.com>
2023-02-21 18:54:19 +0300

    Fix issues statuses does not merge with last update

--- dags/sql/update_issues_status_info.sql
+++ dags/sql/update_issues_status_info.sql
@@ -26,6 +26,21 @@ status_issues as (
 		issues_labels il
 		inner join status_labels sl on il.label_id = sl.id
 		inner join issue_statuses iss on iss.label_id = sl.id
+),
+last_update_issue_statuses as (
+	select
+		t.issue_id,
+		t.status_id,
+		t.started_at,
+		t.updated_at
+	from (
+		select
+			*,
+			row_number() over (partition by ist.issue_id order by ist.updated_at desc) as rn
+		from
+			issues_status_transition ist
+	)t
+	where rn = 1
 )
 insert into issues_status_transition (
 	issue_id,
@@ -38,11 +53,12 @@ select
 	ti.id as issue_id,
 	ti.team_id as team_id,
 	si.status_id as status_id,
-	'{{ data_interval_end }}'::timestamptz as started_at,
+	coalesce(luis.started_at, '{{ data_interval_end }}'::timestamptz) as started_at,
 	'{{ data_interval_end }}'::timestamptz as updated_at
 from
 	team_issues ti
 	inner join status_issues si on ti.id = si.id
+	left join last_update_issue_statuses luis on ti.id = luis.issue_id and si.status_id = luis.status_id
 on conflict (issue_id, status_id, started_at) do update
 set
-	updated_at = EXCLUDED.updated_at;
+	updated_at = excluded.updated_at;
